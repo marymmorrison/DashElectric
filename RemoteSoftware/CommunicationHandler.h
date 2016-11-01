8ead150eefcb585603824e5f99c457a4181ede62
melloa <mello.a@husky.neu.edu>
2016-11-01 12:38:32 -0400

    Added ISR, check buffer, and process cmd functions

--- RemoteSoftware/CommunicationHandler.h
+++ RemoteSoftware/CommunicationHandler.h
@@ -11,6 +11,13 @@ class CommunicationHandler
 {
 public:
 	CommunicationHandler();
+
+	bool init() {
+		// setup interrupts
+		attachInterrupt(digitalPinToInterrupt(nrf24Interrupt), interruptServiceRoutine, HIGH);
+		return nrf24.init();
+	}
+
 	void sendMessage(String command, String argument) {
 		String str = "$" + command + ":" + argument + ";";
 		int8_t data[sizeof(str)];
@@ -21,39 +28,59 @@ public:
 		nrf24.waitPacketSent();
 	}
 
-	void receive() // THIS NEEDS WORK !
-	{
-		if (nrf24.available())
+	bool available() {
+		if (!nrf24.available())
 		{
-			String readString;
-			int start = readString.lastIndexOf('$');
-			int seperator = readString.indexOf(':', start);
-			int end = readString.indexOf(';', start);
-
-			String cmd = readString.substring(start + 1, seperator);
-			String arg = readString.substring(seperator + 1, end);
-
-			logger.verbose("Received: " + readString);
-			logger.verbose("\t Command:" + cmd + " Argument:" + arg);
+			logger.warn("Wireless communication not available")
+			return false;
 		}
+		return bufferIndex > 0;
 	}
 
-	String interruptServiceRoutine() {
+	void processCommand()
+	{
+
+		String readString = buffer[bufferIndex];
+		buffer[bufferIndex] = "";
+		bufferIndex--;
+
+		int start = readString.lastIndexOf('$');
+		int seperator = readString.indexOf(':', start);
+		int end = readString.indexOf(';', start);
+
+		String cmd = readString.substring(start + 1, seperator);
+		String arg = readString.substring(seperator + 1, end);
+
+		logger.verbose("Received: " + readString);
+		logger.verbose("\t Command:" + cmd + " Argument:" + arg);
+	}
+
+
+private:
+	NRF24 nrf24; // Singleton instance of the radio driver
+	Logger logger;
+	volatile String buffer[16];
+	volatile int bufferIndex = 0;
+
+	void interruptServiceRoutine() {
 		uint8_t buf[128]; //RH_NRF24_MAX_MESSAGE_LEN ???
 		uint8_t len = sizeof(buf);
 		if (nrf24.recv(buf, &len))
 		{
 			String readString = (char*)buf;
+			if(bufferIndex < sizeof(buffer))
+			{
+				buffer[bufferIndex] = readString;
+				bufferIndex++;
+			}
+			else
+			{
+				logger.error("Buffer overflow from communication ISR!")
+			}
 			logger.info("Message received: " + readString);
-			return readString;
 		}
-		logger.error("Null message received on wireless communication ISR!");
-		return "";
+		logger.warn("Null message received on wireless communication ISR.");
 	}
-
-private:
-	NRF24 nrf24; // Singleton instance of the radio driver
-	Logger logger;
 };
 
 #endif
